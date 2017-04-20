ea77a1ea51eaf3bbd475736be479d8e9b2a9a599
Javi Perán <javperan@gmail.com>
2017-04-20 10:55:10 +0200

    Termina interfaz. Todo ok

--- app/src/main/java/com/example/javi/pruebavolley2/MainActivity.java
+++ app/src/main/java/com/example/javi/pruebavolley2/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.javi.pruebavolley2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +18,35 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.Toolbar);
         setSupportActionBar(toolbar);
 
+        RecyclerView myRecyclerview = (RecyclerView) findViewById(R.id.MyRecyclerView);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+
+
+            case R.id.about_action:
+
+                Toast.makeText(this, "Has pulsado Acerca de....", Toast.LENGTH_SHORT).show();
+
+            case R.id.refresh_action:
+
+                Toast.makeText(this, "Has pulsado Actualizar", Toast.LENGTH_SHORT).show();
+
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
