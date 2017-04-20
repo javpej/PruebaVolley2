d3c20e38237476f1c0bc8ecc3e643d3a75748bb9
Javi Perán <javperan@gmail.com>
2017-04-20 11:20:28 +0200

    Interfaz Funcionando. Tarjetas Montonadas

--- app/src/main/java/com/example/javi/pruebavolley2/MainActivity.java
+++ app/src/main/java/com/example/javi/pruebavolley2/MainActivity.java
@@ -2,13 +2,18 @@ package com.example.javi.pruebavolley2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<Item> items;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +24,18 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         RecyclerView myRecyclerview = (RecyclerView) findViewById(R.id.MyRecyclerView);
+
+        items = new ArrayList<Item>();
+
+        Item item1 = new Item("Lorca", "20 ºC", "Nublado");
+        items.add(item1);
+
+        Item item2 = new Item("Murcia", "25 ºC", "Soleado");
+        items.add(item2);
+
+        Adapter myAdapter = new Adapter(items, this);
+        myRecyclerview.setAdapter(myAdapter);
+        myRecyclerview.setLayoutManager(new LinearLayoutManager(this));
 
     }
 
